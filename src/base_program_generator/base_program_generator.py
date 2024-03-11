cee219ad74f6856ecf0b7cbc63e794d2f8a1650c
eugenetangkj <dragoneugene10@gmail.com>
2024-03-11 18:37:10 +0800

    Add imports

--- src/base_program_generator/base_program_generator.py
+++ src/base_program_generator/base_program_generator.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class BaseProgramGenerator(object):
     """
     This is an abstract class from which concrete program generator classes would extend from. Subclasses that extend
@@ -12,4 +14,18 @@ class BaseProgramGenerator(object):
         """
         pass
     
-    ## Insert other methods of BaseProgramGenerator here
+    @abstractmethod
+    def generate_test_case(self, language, constraints):
+        """
+        Abstract method to be overridden by concrete program generator classes.
+        This method aims to generate a random base program written in a given language with
+        specific constraints to guide the creation of the base program.
+
+        Parameters:
+            language: A string indicating the language in which the base program is written in.
+            constraints: A string indicating some constraints and guidelines to generate the base program with.
+
+        """
+        pass
+
+
--- src/base_program_generator/openai_program_generator.py
+++ src/base_program_generator/openai_program_generator.py
@@ -1,4 +1,8 @@
 from .base_program_generator import BaseProgramGenerator
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv() # Loads environment variables for OpenAI API key
 
 class OpenAiProgramGenerator(BaseProgramGenerator):
     """
@@ -13,4 +17,21 @@ class OpenAiProgramGenerator(BaseProgramGenerator):
         """
         pass
     
-    ## Insert other methods of OpenAiProgramGenerator here
+
+    def generate_test_case(self, language, constraints):
+        """
+        Generates a random base program written in a given language with
+        specific constraints to guide the creation of the base program.
+
+        Parameters:
+            language: A string indicating the language in which the base program is written in.
+            constraints: A string indicating some constraints and guidelines to generate the base program with.
+
+        """
+
+        # TODO: Implement logic for generating test case
+        pass
+
+
+
+
