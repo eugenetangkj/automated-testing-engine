f2bbae7029e5bad229b336f8acd7277fbb248fda
toh-xinyi <tohxinyi10@gmail.com>
2024-03-31 00:42:40 +0800

    Fix bug

--- tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
+++ tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
@@ -1,16 +1,17 @@
+import json
 from its_test_engine.enums import Language
 from its_test_engine.its.its_api_connection import ItsApiConnection
 from its_test_engine.its.endpoint_tester import ErrorLocalizerEndpointTester
 
 
 PROGRAM_TO_SUCCESS = "def add(a, b): return a + b"
-PROGRAM_TO_FAIL = "def add(a, b): return b"
+PROGRAM_TO_FAIL = "def add1(a, b): return b"
 
 
 class MockItsApiConnection(ItsApiConnection):
     def call_errorlocalizer_endpoint(self, request_payload: dict):
         ref_solution = request_payload["reference_solution"]
-        if ref_solution == PROGRAM_TO_FAIL:
+        if ref_solution == json.dumps(PROGRAM_TO_FAIL):
             raise Exception("Invalid reference solution")
         return {"errorLocations": {}}
     
@@ -26,7 +27,7 @@ def test_error_localizer_endpoint_tester():
     assert test_result.success
 
     test_result = error_localizer_tester.run_test(
-        {"name":"add"}, "", "", PROGRAM_TO_FAIL, PROGRAM_TO_FAIL, []
+        {"name":"add1"}, "", "", PROGRAM_TO_FAIL, PROGRAM_TO_FAIL, []
     )
     assert not test_result.success
     
