6603cc7f3fddce8a473f09cd04b518a0c0b83be7
Tex Tang <tex_tang@outlook.com>
2024-03-30 13:57:17 +0800

    Add tests for parser and interpreter

---
+++ tests/its/endpoint_tester/test_interpreter_tester.py
@@ -0,0 +1,33 @@
+from its_test_engine.enums import Language
+from its_test_engine.its.its_api_connection import ItsApiConnection
+from its_test_engine.its.endpoint_tester import InterpreterTester
+
+
+PROGRAM_TO_SUCCESS = "def add(a, b): return a + b"
+PROGRAM_TO_FAIL = "def add2(a, b): return a"
+
+
+class MockItsApiConnection(ItsApiConnection):
+    def call_interpreter_endpoint(self, payload: dict):
+        function = payload["function"]
+        if function == "add2":
+            raise Exception("Invalid program")
+        return {"entries": [{"functionName": function}]}
+
+
+def test_parser_tester():
+    its_api_connection = MockItsApiConnection(Language.PYTHON)
+
+    interpreter_tester = InterpreterTester(its_api_connection)
+
+    interpreter_output, test_result = interpreter_tester.run_test(
+        {"name": "add"}, PROGRAM_TO_SUCCESS, [1, 2]
+    )
+    assert interpreter_output == {"entries": [{"functionName": "add"}]}
+    assert test_result.success
+
+    interpreter_output, test_result = interpreter_tester.run_test(
+        {"name": "add2"}, PROGRAM_TO_FAIL, [1, 2]
+    )
+    assert interpreter_output is None
+    assert not test_result.success
