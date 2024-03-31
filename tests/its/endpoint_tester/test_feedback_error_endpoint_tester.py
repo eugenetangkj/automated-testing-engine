427e4a10f1920b4c4125b85cba124f83451a45fa
Joy <foojnjoy@gmail.com>
2024-03-31 18:48:29 +0800

    Make requested changes. Add variant test case so now coverage is at 100% :D!

--- tests/its/endpoint_tester/test_feedback_error_endpoint_tester.py
+++ tests/its/endpoint_tester/test_feedback_error_endpoint_tester.py
@@ -4,14 +4,18 @@ from its_test_engine.its.its_api_connection import ItsApiConnection
 from its_test_engine.its.endpoint_tester import FeedbackErrorEndpointTester
 
 BASE_PROGRAM = "def add(a, b): a + b"
+PARSED_BASE_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add\": {\"name\": \"add\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add'\"}, \"types\": {}}}}"
+
 EQUIVALENT_PROGRAM = "def add1(a, b): return a - -b"
+PARSED_EQUIVALENT_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add1\": {\"name\": \"add1\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add1'\"}, \"types\": {}}}}"
+
 VARIANT_PROGRAM = "def add2(a, b): return a - b"
-FAIL_PROGRAM = "def add3(a, b): return a"
+PARSED_VARIANT_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add2\": {\"name\": \"add2\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add2'\"}, \"types\": {}}}}"
+
+FAIL_PROGRAM = "def add3(a, b): return b"
+PARSED_FAIL_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add3\": {\"name\": \"add3\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add3'\"}, \"types\": {}}}}"
 
 class MockItsApiConnection(ItsApiConnection):
-    def parse_program(self, program: str):
-        parser_payload = ItsApiConnection.create_parser_request_payload(self, program)
-        return ItsApiConnection.call_parser_endpoint(self, parser_payload)
 
     def call_feedback_error_endpoint(self, request_payload: dict):
         function_name = request_payload["function"]
@@ -20,43 +24,61 @@ class MockItsApiConnection(ItsApiConnection):
 
         if function_name == "add2":
             return [
-                {
-                    "lineNumber": 2,
-                    "oldExpr": "a - b",
-                    "newExpr": "a + b",
-                    "repairStrings": [
-                        "Wrong expression. Change a - b to a + b;"
+                        {
+                            "lineNumber": 2,
+                            "hintStrings": [
+                            "Incorrect expression"
+                            ]
+                        }
                     ]
-                }
-            ]
 
         if function_name == "add3":
             raise Exception("Invalid program")
 
 def test_feedback_error_equivalent_tester():
     its_api_connection = MockItsApiConnection(Language.PYTHON)
-    parsed_base_program = its_api_connection.parse_program(BASE_PROGRAM)
-    parsed_equivalent_program = its_api_connection.parse_program(EQUIVALENT_PROGRAM)
-
+   
     feedback_error_tester = FeedbackErrorEndpointTester(its_api_connection)
     feedback_error_tester.type_of_metamorphic_relation = TypeOfMetamorphicRelation.EQUIVALENT
 
     test_result = feedback_error_tester.run_test(
-        {"name": "add1"}, BASE_PROGRAM, EQUIVALENT_PROGRAM, parsed_base_program, parsed_equivalent_program, [1, 2]
+        {"name": "add1", "argument_types": ["int"], "return_type": "int"},
+            BASE_PROGRAM,
+            EQUIVALENT_PROGRAM,
+            PARSED_BASE_PROGRAM,
+            PARSED_EQUIVALENT_PROGRAM,
+            []
     )
 
     assert test_result.success
 
+def test_feedback_fix_variant_tester():
+    its_api_connection = MockItsApiConnection(Language.PYTHON)
+
+    feedback_fix_tester = FeedbackErrorEndpointTester(its_api_connection, TypeOfMetamorphicRelation.VARIANT)
+
+    test_result = feedback_fix_tester.run_test(
+        {"name":"add2", "argument_types": ["int"], "return_type": "int"},
+            BASE_PROGRAM,
+            VARIANT_PROGRAM,
+            PARSED_BASE_PROGRAM,
+            PARSED_VARIANT_PROGRAM,
+            []
+    )
+    assert test_result.success
+
 def test_feedback_error_exception_tester():
     its_api_connection = MockItsApiConnection(Language.PYTHON)
-    parsed_base_program = its_api_connection.parse_program(BASE_PROGRAM)
-    parsed_fail_program = its_api_connection.parse_program(FAIL_PROGRAM)
-
+   
     feedback_error_tester = FeedbackErrorEndpointTester(its_api_connection)
-    feedback_error_tester.type_of_metamorphic_relation = TypeOfMetamorphicRelation.VARIANT
 
     test_result = feedback_error_tester.run_test(
-        {"name": "add3"}, BASE_PROGRAM, FAIL_PROGRAM, parsed_base_program, parsed_fail_program, [1, 2]
+        {"name": "add3", "argument_types": ["int"], "return_type": "int"},
+            BASE_PROGRAM,
+            FAIL_PROGRAM,
+            PARSED_BASE_PROGRAM,
+            PARSED_FAIL_PROGRAM,
+            []
     )
 
     assert not test_result.success
