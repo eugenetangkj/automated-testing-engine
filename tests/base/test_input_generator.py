0a5eb3d6fa22c1a8155776636facd17ccea0dd2b
Tex Tang <tex_tang@outlook.com>
2024-03-31 19:02:09 +0800

    Add tests for input_generator

--- tests/base/test_input_generator.py
+++ tests/base/test_input_generator.py
@@ -28,9 +28,56 @@ def test_random_input_generator():
     )
 
     assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], int) for i in inputs)
+    assert all(isinstance(i[1], int) for i in inputs)
 
     inputs = input_generator.generate_inputs(
         {"name": "add", "argument_types": ["int", "int"]}, COMPLICATED_CODE, 20
     )
 
     assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], int) for i in inputs)
+    assert all(isinstance(i[1], int) for i in inputs)
+
+    inputs = input_generator.generate_inputs(
+        {"name": "add", "argument_types": ["float", "bool"]}, CODE, 20
+    )
+
+    assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], float) for i in inputs)
+    assert all(isinstance(i[1], bool) for i in inputs)
+
+    inputs = input_generator.generate_inputs(
+        {"name": "add", "argument_types": ["str", "List[int]"]}, CODE, 20
+    )
+
+    assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], str) for i in inputs)
+    assert all(isinstance(i[1], list) for i in inputs)
+    assert all(all(isinstance(j, int) for j in i[1]) for i in inputs)
+
+    inputs = input_generator.generate_inputs(
+        {"name": "add", "argument_types": ["List[float]", "List[str]"]}, CODE, 20
+    )
+
+    assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], list) for i in inputs)
+    assert all(isinstance(i[1], list) for i in inputs)
+    assert all(all(isinstance(j, float) for j in i[0]) for i in inputs)
+    assert all(all(isinstance(j, str) for j in i[1]) for i in inputs)
+
+    inputs = input_generator.generate_inputs(
+        {"name": "add", "argument_types": ["List[bool]", "List[bool]"]}, CODE, 20
+    )
+
+    assert len(inputs) == 20
+    assert all(len(i) == 2 for i in inputs)
+    assert all(isinstance(i[0], list) for i in inputs)
+    assert all(isinstance(i[1], list) for i in inputs)
+    assert all(all(isinstance(j, bool) for j in i[0]) for i in inputs)
+    assert all(all(isinstance(j, bool) for j in i[1]) for i in inputs)
