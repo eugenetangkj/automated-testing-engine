fbd865ff948402b48e89fbb9f8c9919ac7ee3be4
toh-xinyi <tohxinyi10@gmail.com>
2024-03-31 16:28:14 +0800

    Edit test case to reduce dependency on ITS API

--- tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
+++ tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
@@ -4,18 +4,20 @@ from its_test_engine.its.its_api_connection import ItsApiConnection
 from its_test_engine.its.endpoint_tester import ErrorLocalizerEndpointTester
 
 BASE_PROGRAM = "def add(a, b): return a + b"
+PARSED_BASE_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add\": {\"name\": \"add\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add'\"}, \"types\": {}}}}"
+
 PROGRAM_TO_GIVE_SUCCESSFUL = "def add1(a, b): return a + b"
+PARSED_PROGRAM_TO_GIVE_SUCCESSFUL = "{\"importStatements\": [], \"fncs\": {\"add1\": {\"name\": \"add1\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add'\"}, \"types\": {}}}}"
+
 PROGRAM_TO_GIVE_UNSUCCESSFUL = "def add2(a, b): return b"
+PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL = "{\"importStatements\": [], \"fncs\": {\"add2\": {\"name\": \"add2\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add2'\"}, \"types\": {}}}}"
+
 
 
 class MockItsApiConnection(ItsApiConnection):
-    def get_parsed_output_for_test(self, source_code):
-        payload = self.create_parser_request_payload(source_code)
-        return json.dumps(self.call_parser_endpoint(payload))
-
     def call_errorlocalizer_endpoint(self, request_payload: dict):
         student_solution = request_payload["student_solution"]
-        expected = json.dumps(self.get_parsed_output_for_test(PROGRAM_TO_GIVE_UNSUCCESSFUL))
+        expected = json.dumps(PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL)
         if student_solution == expected:
             raise Exception("Give unsuccessful test result")
         return {"errorLocations": {}}
@@ -26,16 +28,12 @@ def test_error_localizer_endpoint_tester():
 
     error_localizer_tester = ErrorLocalizerEndpointTester(its_api_connection)
 
-    parsed_base = its_api_connection.get_parsed_output_for_test(BASE_PROGRAM)
-    parsed_successful = its_api_connection.get_parsed_output_for_test(PROGRAM_TO_GIVE_SUCCESSFUL)
-    parsed_unsuccessful = its_api_connection.get_parsed_output_for_test(PROGRAM_TO_GIVE_UNSUCCESSFUL)
-
     test_result = error_localizer_tester.run_test(
         {"name":"add", "argument_types": ["int"], "return_type": "int"}, 
         BASE_PROGRAM, 
         PROGRAM_TO_GIVE_SUCCESSFUL, 
-        parsed_base, 
-        parsed_successful, 
+        PARSED_BASE_PROGRAM, 
+        PARSED_PROGRAM_TO_GIVE_SUCCESSFUL, 
         []
     )
     assert test_result.success
@@ -44,8 +42,8 @@ def test_error_localizer_endpoint_tester():
         {"name":"add1", "argument_types": ["int"], "return_type": "int"},
         BASE_PROGRAM, 
         PROGRAM_TO_GIVE_UNSUCCESSFUL, 
-        parsed_base,
-        parsed_unsuccessful,
+        PARSED_BASE_PROGRAM,
+        PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL,
         []
     )
     assert not test_result.success
