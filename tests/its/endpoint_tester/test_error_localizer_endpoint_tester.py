665acaf3924a650b6d04d931d83486520a0f0e0d
toh-xinyi <tohxinyi10@gmail.com>
2024-03-31 15:16:54 +0800

    Edit test cases

--- tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
+++ tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
@@ -3,16 +3,21 @@ from its_test_engine.enums import Language
 from its_test_engine.its.its_api_connection import ItsApiConnection
 from its_test_engine.its.endpoint_tester import ErrorLocalizerEndpointTester
 
-
-PROGRAM_TO_SUCCESS = "def add(a, b): return a + b"
-PROGRAM_TO_FAIL = "def add1(a, b): return b"
+BASE_PROGRAM = "def add(a, b): return a + b"
+PROGRAM_TO_GIVE_SUCCESSFUL = "def add1(a, b): return a + b"
+PROGRAM_TO_GIVE_UNSUCCESSFUL = "def add2(a, b): return b"
 
 
 class MockItsApiConnection(ItsApiConnection):
+    def get_parsed_output_for_test(self, source_code):
+        payload = self.create_parser_request_payload(source_code)
+        return json.dumps(self.call_parser_endpoint(payload))
+
     def call_errorlocalizer_endpoint(self, request_payload: dict):
-        ref_solution = request_payload["reference_solution"]
-        if ref_solution == json.dumps(PROGRAM_TO_FAIL):
-            raise Exception("Invalid reference solution")
+        student_solution = request_payload["student_solution"]
+        expected = json.dumps(self.get_parsed_output_for_test(PROGRAM_TO_GIVE_UNSUCCESSFUL))
+        if student_solution == expected:
+            raise Exception("Give unsuccessful test result")
         return {"errorLocations": {}}
     
 
@@ -21,13 +26,27 @@ def test_error_localizer_endpoint_tester():
 
     error_localizer_tester = ErrorLocalizerEndpointTester(its_api_connection)
 
+    parsed_base = its_api_connection.get_parsed_output_for_test(BASE_PROGRAM)
+    parsed_successful = its_api_connection.get_parsed_output_for_test(PROGRAM_TO_GIVE_SUCCESSFUL)
+    parsed_unsuccessful = its_api_connection.get_parsed_output_for_test(PROGRAM_TO_GIVE_UNSUCCESSFUL)
+
     test_result = error_localizer_tester.run_test(
-        {"name":"add"}, "", "", PROGRAM_TO_SUCCESS, PROGRAM_TO_SUCCESS, []
+        {"name":"add", "argument_types": ["int"], "return_type": "int"}, 
+        BASE_PROGRAM, 
+        PROGRAM_TO_GIVE_SUCCESSFUL, 
+        parsed_base, 
+        parsed_successful, 
+        []
     )
     assert test_result.success
 
     test_result = error_localizer_tester.run_test(
-        {"name":"add1"}, "", "", PROGRAM_TO_FAIL, PROGRAM_TO_FAIL, []
+        {"name":"add1", "argument_types": ["int"], "return_type": "int"},
+        BASE_PROGRAM, 
+        PROGRAM_TO_GIVE_UNSUCCESSFUL, 
+        parsed_base,
+        parsed_unsuccessful,
+        []
     )
     assert not test_result.success
     
