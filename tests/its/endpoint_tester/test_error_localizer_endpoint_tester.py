74cf38ddf4481f735069bf34b787c234b4e268c8
Tex Tang <tex_tang@outlook.com>
2024-03-31 18:05:11 +0800

    Add variant error localizer endpoint tester

--- tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
+++ tests/its/endpoint_tester/test_error_localizer_endpoint_tester.py
@@ -1,17 +1,16 @@
 import json
-from its_test_engine.enums import Language
+from its_test_engine.enums import Language, TypeOfMetamorphicRelation
 from its_test_engine.its.its_api_connection import ItsApiConnection
 from its_test_engine.its.endpoint_tester import ErrorLocalizerEndpointTester
 
 BASE_PROGRAM = "def add(a, b): return a + b"
-PARSED_BASE_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add\": {\"name\": \"add\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add'\"}, \"types\": {}}}}"
+PARSED_BASE_PROGRAM = '{"importStatements": [], "fncs": {"add": {"name": "add", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add\'"}, "types": {}}}}'
 
 PROGRAM_TO_GIVE_SUCCESSFUL = "def add1(a, b): return a + b"
-PARSED_PROGRAM_TO_GIVE_SUCCESSFUL = "{\"importStatements\": [], \"fncs\": {\"add1\": {\"name\": \"add1\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add'\"}, \"types\": {}}}}"
+PARSED_PROGRAM_TO_GIVE_SUCCESSFUL = '{"importStatements": [], "fncs": {"add1": {"name": "add1", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add\'"}, "types": {}}}}'
 
 PROGRAM_TO_GIVE_UNSUCCESSFUL = "def add2(a, b): return b"
-PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL = "{\"importStatements\": [], \"fncs\": {\"add2\": {\"name\": \"add2\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add2'\"}, \"types\": {}}}}"
-
+PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL = '{"importStatements": [], "fncs": {"add2": {"name": "add2", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add2\'"}, "types": {}}}}'
 
 
 class MockItsApiConnection(ItsApiConnection):
@@ -21,30 +20,47 @@ class MockItsApiConnection(ItsApiConnection):
         if student_solution == expected:
             raise Exception("Give unsuccessful test result")
         return {"errorLocations": {}}
-    
 
-def test_error_localizer_endpoint_tester():
+
+def test_equivalent_error_localizer_endpoint_tester():
     its_api_connection = MockItsApiConnection(Language.PYTHON)
 
     error_localizer_tester = ErrorLocalizerEndpointTester(its_api_connection)
 
     test_result = error_localizer_tester.run_test(
-        {"name":"add", "argument_types": ["int"], "return_type": "int"}, 
-        BASE_PROGRAM, 
-        PROGRAM_TO_GIVE_SUCCESSFUL, 
-        PARSED_BASE_PROGRAM, 
-        PARSED_PROGRAM_TO_GIVE_SUCCESSFUL, 
-        []
+        {"name": "add", "argument_types": ["int"], "return_type": "int"},
+        BASE_PROGRAM,
+        PROGRAM_TO_GIVE_SUCCESSFUL,
+        PARSED_BASE_PROGRAM,
+        PARSED_PROGRAM_TO_GIVE_SUCCESSFUL,
+        [],
     )
     assert test_result.success
 
     test_result = error_localizer_tester.run_test(
-        {"name":"add1", "argument_types": ["int"], "return_type": "int"},
-        BASE_PROGRAM, 
-        PROGRAM_TO_GIVE_UNSUCCESSFUL, 
+        {"name": "add1", "argument_types": ["int"], "return_type": "int"},
+        BASE_PROGRAM,
+        PROGRAM_TO_GIVE_UNSUCCESSFUL,
         PARSED_BASE_PROGRAM,
         PARSED_PROGRAM_TO_GIVE_UNSUCCESSFUL,
-        []
+        [],
     )
     assert not test_result.success
-    
+
+
+def test_variant_error_localizer_endpoint_tester():
+    its_api_connection = MockItsApiConnection(Language.PYTHON)
+
+    error_localizer_tester = ErrorLocalizerEndpointTester(
+        its_api_connection, TypeOfMetamorphicRelation.VARIANT
+    )
+
+    test_result = error_localizer_tester.run_test(
+        {"name": "add", "argument_types": ["int"], "return_type": "int"},
+        BASE_PROGRAM,
+        PROGRAM_TO_GIVE_SUCCESSFUL,
+        PARSED_BASE_PROGRAM,
+        PARSED_PROGRAM_TO_GIVE_SUCCESSFUL,
+        [],
+    )
+    assert not test_result.success
