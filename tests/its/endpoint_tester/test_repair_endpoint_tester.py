1a689642f78c57d8155107b709e28fc74e418231
Sara Han <sarahan716@gmail.com>
2024-04-01 14:30:37 +0800

    updated to make one successful function and one varient function

--- tests/its/endpoint_tester/test_repair_endpoint_tester.py
+++ tests/its/endpoint_tester/test_repair_endpoint_tester.py
@@ -6,16 +6,17 @@ from its_test_engine.its.endpoint_tester import RepairEndpointTester
 BASE_PROGRAM = "def add(a, b): return a + b"
 PARSED_BASE_PROGRAM = '{"importStatements": [], "fncs": {"add": {"name": "add", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add\'"}, "types": {}}}}'
 
-PROGRAM_SUCCESSFUL = "def add1(a, b): return a + b"
-PARSED_PROGRAM_SUCCESSFUL = '{"importStatements": [], "fncs": {"add1": {"name": "add1", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add\'"}, "types": {}}}}'
+SUCCESS_PROGRAM = "def add1(a, b): return a - -b"
+PARSED_SUCCESS_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add1\": {\"name\": \"add1\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add1'\"}, \"types\": {}}}}"
 
-PROGRAM_UNSUCCESSFUL = "def add2(a, b): return b"
-PARSED_PROGRAM_UNSUCCESSFUL = '{"importStatements": [], "fncs": {"add2": {"name": "add2", "rettype": "*", "initloc": 1, "endloc": 0, "params": [{"val0": "a", "val1": "*", "valueArray": ["a", "*"], "valueList": ["a", "*"]}, {"val0": "b", "val1": "*", "valueArray": ["b", "*"], "valueList": ["b", "*"]}], "locexprs": {"1": []}, "loctrans": {"1": {}}, "locdescs": {"1": "around the beginning of function \'add2\'"}, "types": {}}}}'
+FAIL_PROGRAM = "def add2(a, b): return b"
+PARSED_FAIL_PROGRAM = "{\"importStatements\": [], \"fncs\": {\"add3\": {\"name\": \"add3\", \"rettype\": \"*\", \"initloc\": 1, \"endloc\": 0, \"params\": [{\"val0\": \"a\", \"val1\": \"*\", \"valueArray\": [\"a\", \"*\"], \"valueList\": [\"a\", \"*\"]}, {\"val0\": \"b\", \"val1\": \"*\", \"valueArray\": [\"b\", \"*\"], \"valueList\": [\"b\", \"*\"]}], \"locexprs\": {\"1\": []}, \"loctrans\": {\"1\": {}}, \"locdescs\": {\"1\": \"around the beginning of function 'add3'\"}, \"types\": {}}}}"
+
 
 class MockItsApiConnection(ItsApiConnection):
     def call_repair_endpoint(self, request_payload: dict):
         function_name = request_payload["function"]
-        unsuccessful = json.dumps(PARSED_PROGRAM_UNSUCCESSFUL)
+        unsuccessful = json.dumps(PARSED_FAIL_PROGRAM)
         if function_name == unsuccessful:
             raise Exception("Unsuccessful test result")
         return {"totalCost": 0.0, "localRepairs": {}}
@@ -28,22 +29,12 @@ def test_repair_equivalent_tester():
     test_result = repair_tester.run_test(
         {"name":"add1", "argument_types": ["int"], "return_type": "int"},
         BASE_PROGRAM,
-        PROGRAM_SUCCESSFUL,
+        SUCCESS_PROGRAM,
         PARSED_BASE_PROGRAM,
-        PARSED_PROGRAM_SUCCESSFUL,
+        PARSED_SUCCESS_PROGRAM,
         [],
     )
     assert test_result.success
-
-    test_result = repair_tester.run_test(
-        {"name": "add1", "argument_types": ["int"], "return_type": "int"},
-        BASE_PROGRAM,
-        PROGRAM_UNSUCCESSFUL,
-        PARSED_BASE_PROGRAM,
-        PARSED_PROGRAM_UNSUCCESSFUL,
-        [],
-    )
-    assert not test_result.success
 
 def test_repair_variant_tester():
     its_api_connection = MockItsApiConnection(Language.PYTHON)
@@ -53,9 +44,9 @@ def test_repair_variant_tester():
     test_result = repair_tester.run_test(
         {"name":"add2", "argument_types": ["int"], "return_type": "int"},
         BASE_PROGRAM,
-        PROGRAM_SUCCESSFUL,
+        FAIL_PROGRAM,
         PARSED_BASE_PROGRAM,
-        PARSED_PROGRAM_SUCCESSFUL,
+        PARSED_FAIL_PROGRAM,
         [],
     )
 
