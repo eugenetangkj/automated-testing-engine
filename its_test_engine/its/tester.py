4706db32d02650db4ad8247f850db6c9904bba27
eugenetangkj <dragoneugene10@gmail.com>
2024-04-14 14:55:18 +0800

    Abstract out number of arguments

--- its_test_engine/its/tester.py
+++ its_test_engine/its/tester.py
@@ -19,6 +19,7 @@ from its_test_engine.its.test_result import (
 
 
 class Tester:
+    NO_OF_ARGUMENTS = 10
     def __init__(
         self,
         language: Language,
@@ -44,9 +45,9 @@ class Tester:
             print(syntax_error)
             return None
 
-        # Step 2: Generate inputs
-        inputs = self.argument_generator.generate_arguments(
-            function_signature, base_code, 10
+        # Step 2: Generate arguments
+        arguments = self.argument_generator.generate_arguments(
+            function_signature, base_code, self.NO_OF_ARGUMENTS
         )
 
         # Step 3: Mutate code
@@ -57,7 +58,7 @@ class Tester:
             # For example, it could have \n and \t in the base program string instead of using newlines and tabs
             print(syntax_error)
             return None
-        # Step 4: Put base and mutated inputs into respective API endpoints
+        # Step 4: Put base and mutated programs into respective API endpoints
         parser_tester = ParserTester(its_api_connection)
         interpreter_tester = InterpreterTester(its_api_connection)
         repair_endpoint_tester = RepairEndpointTester(its_api_connection)
@@ -100,27 +101,27 @@ class Tester:
         ):
             return test_suites
 
-        new_inputs = []
+        new_arguments = []
 
         test_suite = ItsTestSuite(Language.PYTHON, "interpreter", base_code)
         test_suites.append(test_suite)
 
-        for program_input in inputs:
+        for program_argument in arguments:
             test_case = ItsTestCase(base_code)
             test_suite.add_test_case(test_case)
 
             _, interpreter_result = interpreter_tester.run_test(
-                function_signature, parsed_base_program, program_input
+                function_signature, parsed_base_program, program_argument
             )
             test_case.add_result(interpreter_result)
             if interpreter_result.success:
-                new_inputs.append(program_input)
+                new_arguments.append(program_argument)
 
         self.writer.write(test_suite)
 
-        inputs = new_inputs
+        arguments = new_arguments
 
-        # Remove inputs that failed to run in the interpreter
+        # Remove arguments that failed to run in the interpreter
         # This is done to avoid errors in the other endpoints.
 
         endpoint_testers = {
@@ -130,7 +131,7 @@ class Tester:
             "repair": repair_endpoint_tester,
         }
 
-        if len(inputs) == 0:
+        if len(arguments) == 0:
             return test_suites
 
         for endpoint, endpoint_tester in endpoint_testers.items():
@@ -147,7 +148,7 @@ class Tester:
                     modified_code,
                     parsed_base_program,
                     parsed_modified_program,
-                    inputs,
+                    arguments,
                 )
                 test_case.add_result(endpoint_test_result)
 
